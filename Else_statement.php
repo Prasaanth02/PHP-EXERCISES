c690a00de56ab5a0726f47d5f7273557b9ab32f5
Prasaanth02 <prasaanthhari02gmail.com>
2025-12-12 01:09:47 +0530

    updated exercise

--- Else_statement.php
+++ Else_statement.php
@@ -9,7 +9,7 @@ echo'
         <script>
             let age=8;
             if (age>=18){
-                document.write("You are Eligible for voting");
+                document.write("You are Eligible for voting and vote for tamil desiyam");
 
             }else{
                 document.write("You are not Eligible for Voting")
